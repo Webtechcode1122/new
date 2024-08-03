3945ba64e4c721711b4c9c9c004f978df5f9b590
acadhrix2004 <163326329+Webtechcode1122@users.noreply.github.com>
2024-08-03 21:21:05 +0530

    Add files via upload

---
+++ clockani.js
@@ -0,0 +1,44 @@
+
+gsap.from(".page2 h1",{
+ y:300,
+ delay:1,
+ opacity:0,
+ duration:2,    
+ scrollTrigger:".page2",
+})
+gsap.from(".page2 h3",{
+    y:300,
+    delay:1,
+    opacity:0,
+    duration:2, 
+    scrollTrigger:".page2",   
+   })
+   gsap.from(".page2 a",{
+    y:300,
+    delay:1,
+    opacity:0,
+    duration:2,   
+    scrollTrigger:".page2", 
+   })
+   gsap.from(".page3 h1",{
+    y:300,
+    delay:1,
+    opacity:0,
+    duration:2,    
+    scrollTrigger:".page3",
+   })
+   gsap.from(".page3 h3",{
+       y:300,
+       delay:1,
+       opacity:0,
+       duration:2, 
+       scrollTrigger:".page3",   
+      })
+      gsap.from(".page3 a",{
+       y:300,
+       delay:1,
+       opacity:0,
+       duration:2,   
+       scrollTrigger:".page3", 
+      })
+
